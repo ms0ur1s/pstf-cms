90902a622b0323ebe993ddac4157d1bfdc135a95
ms0ur1s <msouris@gmail.com>
2024-02-12 12:06:42 +0000

    fist push

---
+++ src/api/enquiry-form/routes/enquiry-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * enquiry-form router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::enquiry-form.enquiry-form');
---
+++ src/api/enquiry-form/services/enquiry-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * enquiry-form service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::enquiry-form.enquiry-form');
